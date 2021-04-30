e4a3244f56ce933409ad7fdee579611f4675bf5f
paterne <paternenught@gmail.com>
2021-04-30 10:21:51 +0200

    add some in home page

--- src/shared/carouselHome.js
+++ src/shared/carouselHome.js
@@ -1,18 +1,18 @@
 export const ITEMS = [
     {
-        src: 'assets/images/products/carousel1.png',
+        src: 'https://firebasestorage.googleapis.com/v0/b/resto247-2c1f2.appspot.com/o/images%2Fcarousel1.png?alt=media&token=53c32be2-41c2-441e-b6ca-0b0a41b63153',
         altText: 'Slide 1',
         caption: 'Slide 1',
         mainlink: "Order food with us"
     },
     {
-        src: 'assets/images/products/carousel2.jpg',
+        src: 'https://firebasestorage.googleapis.com/v0/b/resto247-2c1f2.appspot.com/o/images%2Fcarousel3.jpg?alt=media&token=36130ad1-00f5-4665-8ec5-5135c4a589a4',
         altText: 'Slide 2',
         caption: 'Slide 2',
         mainlink: "Order food with us"
     },
     {
-        src: 'assets/images/products/carousel3.jpg',
+        src: 'https://firebasestorage.googleapis.com/v0/b/resto247-2c1f2.appspot.com/o/images%2Fcarousel2.jpg?alt=media&token=7bf2eadf-cf30-4c76-a46a-99eaa577cca5',
         altText: 'Slide 3',
         caption: 'Slide 3',
         mainlink: "Order food with us"
